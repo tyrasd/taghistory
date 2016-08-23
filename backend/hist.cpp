6de7650add2b8260ea26947e996be7d12b50a5ff
Martin Raifer <tyr.asd@gmail.com>
2016-08-23 17:15:38 +0200

    option to only produce output starting from a given timestamp

--- backend/hist.cpp
+++ backend/hist.cpp
@@ -1,11 +1,3 @@
-/*
-
-  This is a small tool that counts the number of nodes, ways, and relations in
-  the input file.
-
-  The code in this example file is released into the Public Domain.
-
-*/
 
 #include <cstdint>
 #include <iostream>
@@ -13,6 +5,7 @@
 #include <map>
 #include <unordered_map>
 #include <string>
+#include <time.h>
 
 #include <osmium/io/any_input.hpp>
 #include <osmium/handler.hpp>
@@ -20,37 +13,42 @@
 
 struct CountHandler : public osmium::handler::Handler {
 
-    uint64_t nodes = 0;
-    
-    std::unordered_map<std::string, std::unordered_map<std::string, std::unordered_map<std::string, int>>> map;
+    uint64_t count = 0;
+
+    // set timestamp of last update here, to only get updates since then
+    const time_t update_from = 1468713600; //"2016-07-17T00:00:00.000Z"
+
+    std::unordered_map<std::string, std::unordered_map<std::string, std::unordered_map<std::string, std::unordered_map<std::string, int>>>> map;
 
     std::unordered_map<std::string, std::string> prevtags;
 
-    void way(osmium::Way& obj) {
+    void process(osmium::OSMObject& obj, std::string type) {
 //std::cout << "n" << obj.id() << "/" << obj.version() << "/" << "/" << std::endl;
         static osmium::object_id_type id_;
-        if (id_ != obj.id()) {
-            ++nodes;
-//std::cout << "n#" << nodes << std::endl;
+        bool update_map = obj.timestamp().seconds_since_epoch() > update_from;
+//if (update_map) std::cout << type << "#" << obj.id() << std::endl;// else std::cout << "--#" << count << std::endl;
+        if (id_ != obj.id()) { // todo: compare obj's type also
+            ++count;
+//std::cout << "n#" << count << std::endl;
             prevtags.clear();
             for (const osmium::Tag& tag : obj.tags()) {
                 prevtags[tag.key()] = tag.value();
-                map[tag.key()][tag.value()][obj.timestamp().to_iso().substr(0,10)] += 1;
+                if (update_map) map[type][tag.key()][tag.value()][obj.timestamp().to_iso().substr(0,10)] += 1;
             }
         } else { // object already existed
 //std::cout << "a" << std::endl;
             for(const osmium::Tag& tag : obj.tags()) {
                 if (prevtags.count(tag.key()) == 0) {
                     // new tag
-                    map[tag.key()][tag.value()][obj.timestamp().to_iso().substr(0,10)] += 1;
+                    if (update_map) map[type][tag.key()][tag.value()][obj.timestamp().to_iso().substr(0,10)] += 1;
                     prevtags[tag.key()] = tag.value();
                     //continue;
-                } 
+                }
                 //if (prevtags[tag.key()] == tag.value()) continue; // nothing changed
                 else if (prevtags[tag.key()] != tag.value()) {
                     // tag changed
-                    map[tag.key()][prevtags[tag.key()]][obj.timestamp().to_iso().substr(0,10)] -= 1;
-                    map[tag.key()][tag.value()][obj.timestamp().to_iso().substr(0,10)] += 1;
+                    if (update_map) map[type][tag.key()][prevtags[tag.key()]][obj.timestamp().to_iso().substr(0,10)] -= 1;
+                    if (update_map) map[type][tag.key()][tag.value()][obj.timestamp().to_iso().substr(0,10)] += 1;
                     prevtags[tag.key()] = tag.value();
                     //continue;
                 }
@@ -60,13 +58,23 @@ struct CountHandler : public osmium::handler::Handler {
             for( auto it = prevtags.begin(); it != prevtags.end(); ) {
                 if (!obj.tags().has_key(it->first.data())) {
                     // removed tag
-                    map[it->first][it->second][obj.timestamp().to_iso().substr(0,10)] -= 1;
+                    if (update_map) map[type][it->first][it->second][obj.timestamp().to_iso().substr(0,10)] -= 1;
                     it = prevtags.erase(it);
                 } else ++it;
             }
 //std::cout << "c" << std::endl;
         }
         id_ = obj.id();
+    }
+
+    void node(osmium::Node& obj) {
+      process(obj, "node");
+    }
+    void way(osmium::Way& obj) {
+      process(obj, "way");
+    }
+    void relation(osmium::Relation& obj) {
+      process(obj, "relation");
     }
 
 };
@@ -80,26 +88,28 @@ int main(int argc, char* argv[]) {
     }
 
     osmium::io::File infile(argv[1], ".pbf");
-    osmium::io::Reader reader(infile, osmium::osm_entity_bits::way);
+    osmium::io::Reader reader(infile, osmium::osm_entity_bits::all);
 
     CountHandler handler;
     osmium::apply(reader, handler);
     reader.close();
 
-    std::cerr << "Nodes: "     << handler.nodes << "\n";
+    std::cerr << "processed objects count: "     << handler.count << "\n";
 
-    std::string key,value;
-    for( const auto& k : handler.map ) {
+    std::string type,key,value;
+    for( const auto& t : handler.map ) {
+    for( const auto& k : t.second ) {
     for( const auto& v : k.second ) {
     for( const auto& d : v.second ) {
         if (d.second == 0) continue;
+        type = t.first;
         key = k.first;
         value = v.first;
         std::replace( key.begin(), key.end(), '\r', '?');
         std::replace( key.begin(), key.end(), '\n', '?');
         std::replace( value.begin(), value.end(), '\r', '?');
         std::replace( value.begin(), value.end(), '\n', '?');
-        std::cout << d.first << "T00:00:00.000Z,way,\"" << key << "\",\"" << value << "\"," << d.second << "\n";
-    }}}
+        // todo: replace " with ""
+        std::cout << d.first << "T00:00:00.000Z," << type << ",\"" << key << "\",\"" << value << "\"," << d.second << "\n";
+    }}}}
 }
-
